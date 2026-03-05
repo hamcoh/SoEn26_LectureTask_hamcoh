adec2f9e73c05309028323acccd831dd3f8bf964
Tarek <tarekalakmeh@hotmail.ch>
2026-03-05 13:03:10 +0100

    Initial commit

---
+++ apps/api/src/controllers/auth.controllers.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import { PlatformUser } from "@enterprise-commerce/core/platform/types"
+import { createUser } from "../models/User"
+
+export const registerUser = async (req: Request, res: Response): Promise<void> => {
+  const { email, password } = req.body;
+  const newUser: PlatformUser = {
+    id: null,
+    email,
+    password
+  };
+
+  // please finish this function
+
+};
---
+++ apps/api/src/models/User.ts
@@ -0,0 +1,31 @@
+import sqlite3 from 'sqlite3';
+import { open, Database } from 'sqlite';
+import path from 'path';
+import bcrypt from 'bcryptjs';
+import { PlatformUser } from "@enterprise-commerce/core/platform/types"
+import openDb from '../db/db';
+
+export const createUser = () => {} // Implement the createUser function
+
+export const findUserById = async (id: string): Promise<PlatformUser | null> => {
+  const db = await openDb();
+  const user = await db.get<PlatformUser>('SELECT * FROM users WHERE id = ?', id);
+  await db.close();
+  return user || null;
+};
+
+// The function below might be useful for task 2. You can disregard it for the register function for task 1.
+/**
+ * Compares a plain text password with a hashed password.
+ *
+ * This function uses bcrypt to asynchronously compare a plain text password with a hashed password 
+ * to determine if they match.
+ *
+ * @param {string} password - The plain text password to be compared. (input from user when trying to login)
+ * @param {string} hashedPassword - The hashed password to compare against. (encrypted password stored in database)
+ * @returns {Promise<boolean>} - A promise that resolves to `true` if the passwords match, 
+ *                               and `false` otherwise.
+ */
+export const comparePasswords = async (password: string, hashedPassword: string): Promise<boolean> => {
+  return bcrypt.compare(password, hashedPassword); 
+};
